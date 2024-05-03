3612b4fb5c0d8ef26262f75eb9b5768111d9cca0
universe <hello.its.universe@gmail.com>
2024-05-03 14:36:28 +0900

    initial commit

---
+++ src/main/java/org/iot/hotelitybackend/HotelityBackendApplication.java
@@ -0,0 +1,13 @@
+package org.iot.hotelitybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelityBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iot/hotelitybackend/HotelityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iot.hotelitybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
