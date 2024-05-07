4608f55a2ff040753f6879480556de558bb0082f
universe <hello.its.universe@gmail.com>
2024-05-07 12:19:57 +0900

    modelMapper Bean 추가

--- src/main/java/org/iot/hotelitybackend/HotelityBackendApplication.java
+++ src/main/java/org/iot/hotelitybackend/HotelityBackendApplication.java
@@ -1,13 +1,20 @@
 package org.iot.hotelitybackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HotelityBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelityBackendApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
 	}
 
 }
