65a7e5b8dfd766cccfeef9110dc9d34d9f886909
jhlee <jun73045928@gmail.com>
2024-05-12 17:27:21 +0900

    Feat: "전체 직원 리스트 조회"
    전체 직원 리스트 페이지 별 조회 응답 데이터 추가
    
    권한명, 직책명, 직급명, 부서, 지점명
    ex)
    "nameOfPermission": "ADMIN",
    "nameOfPosition": "파트장",
    "nameOfRank": "대리",
    "nameOfDepartment": "운영팀",
    "nameOfBranch": "본사",
    
    Resolves: #33

--- src/main/java/org/iot/hotelitybackend/employee/dto/EmployeeDTO.java
+++ src/main/java/org/iot/hotelitybackend/employee/dto/EmployeeDTO.java
@@ -4,18 +4,25 @@ import lombok.Data;
 
 @Data
 public class EmployeeDTO {
-	private int employeeCodePk;
-	private String branchCodeFk;
-	private int departmentCodeFk;
-	private String employeeName;
-	private String employeeAddress;
-	private String employeePhoneNumber;
-	private String employeeOfficePhoneNumber;
-	private String employeeEmail;
-	private int positionCodeFk;
-	private int rankCodeFk;
-	private String employeeSystemPassword;
-	private String employeeResignStatus;
-	private int permissionCodeFk;
-	private String employeeProfileImageLink;
+    private int employeeCodePk;
+    private String employeeName;
+    private String employeeAddress;
+    private String employeePhoneNumber;
+    private String employeeOfficePhoneNumber;
+    private String employeeEmail;
+    private String employeeSystemPassword;
+    private String employeeResignStatus;
+    private String employeeProfileImageLink;
+
+    private String nameOfPermission;
+    private String nameOfPosition;
+	private String nameOfRank;
+	private String nameOfDepartment;
+	private String nameOfBranch;
+
+    private int permissionCodeFk;
+    private int positionCodeFk;
+    private int rankCodeFk;
+    private int departmentCodeFk;
+    private String branchCodeFk;
 }
--- src/main/java/org/iot/hotelitybackend/employee/service/EmployeeServiceImpl.java
+++ src/main/java/org/iot/hotelitybackend/employee/service/EmployeeServiceImpl.java
@@ -1,8 +1,10 @@
 package org.iot.hotelitybackend.employee.service;
 
 import org.iot.hotelitybackend.employee.aggregate.EmployeeEntity;
-import org.iot.hotelitybackend.employee.dto.EmployeeDTO;
-import org.iot.hotelitybackend.employee.repository.EmployeeRepository;
+import org.iot.hotelitybackend.employee.dto.*;
+import org.iot.hotelitybackend.employee.repository.*;
+import org.iot.hotelitybackend.hotelmanagement.dto.BranchDTO;
+import org.iot.hotelitybackend.hotelmanagement.repository.BranchRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -20,19 +22,67 @@ import static org.iot.hotelitybackend.common.constant.Constant.*;
 public class EmployeeServiceImpl implements EmployeeService {
     private final ModelMapper mapper;
     private final EmployeeRepository employeeRepository;
+    private final BranchRepository branchRepository;
+    private final DepartmentRepository departmentRepository;
+    private final PermissionRepository permissionRepository;
+    private final RankRepository rankRepository;
+    private final PositionRepository positionRepository;
 
     @Autowired
-    public EmployeeServiceImpl(ModelMapper mapper, EmployeeRepository employeeRepository) {
+    public EmployeeServiceImpl(
+            ModelMapper mapper,
+            EmployeeRepository employeeRepository,
+            BranchRepository branchRepository,
+            DepartmentRepository departmentRepository,
+            PermissionRepository permissionRepository,
+            RankRepository rankRepository,
+            PositionRepository positionRepository
+    ) {
         this.mapper = mapper;
         this.employeeRepository = employeeRepository;
+        this.branchRepository = branchRepository;
+        this.departmentRepository = departmentRepository;
+        this.permissionRepository = permissionRepository;
+        this.rankRepository = rankRepository;
+        this.positionRepository = positionRepository;
     }
 
     @Override
     public Map<String, Object> selectEmployeesList(int pageNum) {
         Pageable pageable = PageRequest.of(pageNum, PAGE_SIZE);
         Page<EmployeeEntity> employeePage = employeeRepository.findAll(pageable);
-        List<EmployeeDTO> employeeDTOList =
-                employeePage.stream().map(employeeEntity -> mapper.map(employeeEntity, EmployeeDTO.class)).toList();
+        List<EmployeeDTO> employeeDTOList = employeePage
+                .stream()
+                .map(employeeEntity -> mapper.map(employeeEntity, EmployeeDTO.class))
+                .peek(employeeDTO -> employeeDTO.setNameOfBranch(mapper.map(
+                        branchRepository.findById(employeeDTO.getBranchCodeFk()), BranchDTO.class).getBranchName()
+                ))
+                .peek(employeeDTO -> employeeDTO.setNameOfRank(mapper.map(
+                        rankRepository.findById(employeeDTO.getRankCodeFk()), RankDTO.class).getRankName()
+                ))
+                .peek(employeeDTO -> employeeDTO.setNameOfPosition(mapper.map(
+                        positionRepository.findById(employeeDTO.getPositionCodeFk()), PositionDTO.class).getPositionName()
+                ))
+                .peek(employeeDTO -> employeeDTO.setNameOfPermission(mapper.map(
+                        permissionRepository.findById(employeeDTO.getPermissionCodeFk()), PermissionDTO.class).getPermissionName()
+                ))
+                .peek(employeeDTO -> employeeDTO.setNameOfDepartment(mapper.map(
+                        departmentRepository.findById(employeeDTO.getDepartmentCodeFk()), DepartmentDTO.class).getDepartmentName()
+                ))
+                .toList();
+
+/*
+List<RoomDTO> roomDTOList = roomEntityPage
+			.stream()
+			.map(roomEntity -> mapper.map(roomEntity, RoomDTO.class))
+			.peek(roomDTO -> roomDTO.setRoomName(
+				mapper.map(roomCategoryRepository.findById(roomDTO.getRoomCategoryCodeFk()), RoomCategoryDTO.class).getRoomName()
+			))
+			.peek(roomDTO -> roomDTO.setBranchName(
+				mapper.map(branchRepository.findById(roomDTO.getBranchCodeFk()), BranchDTO.class).getBranchName()
+			))
+			.toList();
+ */
 
         int totalPagesCount = employeePage.getTotalPages();
         int currentPageIndex = employeePage.getNumber();
