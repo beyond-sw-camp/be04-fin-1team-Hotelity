412ba3fa925fd29ec518349ac5077b4441c24878
seongmin <gam1205@naver.com>
2024-05-15 14:01:34 +0900

    Feat: "부대시설 등록, 수정, 삭제 기능 추가"

--- src/main/java/org/iot/hotelitybackend/hotelmanagement/controller/AncillaryController.java
+++ src/main/java/org/iot/hotelitybackend/hotelmanagement/controller/AncillaryController.java
@@ -4,14 +4,13 @@ import java.util.Map;
 
 import org.iot.hotelitybackend.common.vo.ResponseVO;
 import org.iot.hotelitybackend.hotelmanagement.service.AncillaryService;
+import org.iot.hotelitybackend.hotelmanagement.vo.RequestModifyFacility;
+import org.iot.hotelitybackend.hotelmanagement.vo.RequestRegistFacility;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hotel-management")
@@ -33,6 +32,46 @@ public class AncillaryController {
 			.data(facilityPageInfo)
 			.resultCode(HttpStatus.OK.value())
 			.build();
+
+		return ResponseEntity.status(response.getResultCode()).body(response);
+	}
+
+	@PostMapping("/facilities")
+	public ResponseEntity<ResponseVO> registFacility(RequestRegistFacility requestRegistFacility) {
+		Map<String, Object> registFacilityInfo = ancillaryService.registFacility(requestRegistFacility);
+
+		ResponseVO response = ResponseVO.builder()
+				.data(registFacilityInfo)
+				.resultCode(HttpStatus.CREATED.value())
+				.build();
+
+		return ResponseEntity.status(response.getResultCode()).body(response);
+	}
+
+	@PutMapping("/facilities/{ancillaryCodePk}")
+	public ResponseEntity<ResponseVO> modifyFacility(
+			@RequestBody RequestModifyFacility requestModifyFacility,
+			@PathVariable ("ancillaryCodePk") int ancillaryCodePk
+	) {
+		Map<String, Object> modifyFacilityInfo = ancillaryService.modifyFacilityInfo(requestModifyFacility, ancillaryCodePk);
+
+		ResponseVO response = ResponseVO.builder()
+				.data(modifyFacilityInfo)
+				.resultCode(HttpStatus.CREATED.value())
+				.build();
+
+		return ResponseEntity.status(response.getResultCode()).body(response);
+	}
+
+	@DeleteMapping("/facilities/{ancillaryCodePk}")
+	public ResponseEntity<ResponseVO> deleteFacility(@PathVariable("ancillaryCodePk") int ancillaryCodePk) {
+
+		Map<String, Object> deleteFacilityInfo = ancillaryService.deleteFacilityInfo(ancillaryCodePk);
+
+		ResponseVO response = ResponseVO.builder()
+				.data(deleteFacilityInfo)
+				.resultCode(HttpStatus.NO_CONTENT.value())
+				.build();
 
 		return ResponseEntity.status(response.getResultCode()).body(response);
 	}
