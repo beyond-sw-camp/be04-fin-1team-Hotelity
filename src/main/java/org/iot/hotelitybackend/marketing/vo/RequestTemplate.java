86843cf42c183dacd159de7fa4886e180d2d1cdb
seongmin <gam1205@naver.com>
2024-05-15 13:21:15 +0900

    Feat: "템플릿 등록, 수정, 삭제 기능 추가"

---
+++ src/main/java/org/iot/hotelitybackend/marketing/vo/RequestTemplate.java
@@ -0,0 +1,9 @@
+package org.iot.hotelitybackend.marketing.vo;
+
+import lombok.Data;
+
+@Data
+public class RequestTemplate {
+    private String templateName;
+    private String templateContent;
+}
