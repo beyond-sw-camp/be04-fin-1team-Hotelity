803834c44ed6615069faac3a2e5735c998216362
universe <hello.its.universe@gmail.com>
2024-05-21 17:35:38 +0900

    Feat: "VOC 엑셀 다운로드"
    VOC 엑셀 다운로드 구현 완료

--- src/main/java/org/iot/hotelitybackend/sales/service/VocService.java
+++ src/main/java/org/iot/hotelitybackend/sales/service/VocService.java
@@ -3,7 +3,9 @@ package org.iot.hotelitybackend.sales.service;
 import org.iot.hotelitybackend.sales.dto.VocDTO;
 import org.iot.hotelitybackend.sales.vo.RequestReplyVoc;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 public interface VocService {
@@ -14,4 +16,11 @@ public interface VocService {
     Map<String, Object> replyVoc(RequestReplyVoc requestReplyVoc, int vocCodePk);
 
     Map<String, Object> selectSearchedVocsList(int pageNum, String branchCodeFk, Integer vocProcessStatus, String vocCategory, Date vocCreatedDate, Integer customerCodeFk);
+
+	List<VocDTO> selectVocsListForExcel();
+
+	Map<String, Object> createVocsExcelFile(List<VocDTO> vocDTOList) throws
+		IOException,
+		NoSuchFieldException,
+		IllegalAccessException;
 }
