ecb7500f13188524f89345cb88df826a483f0425
jhlee <jun73045928@gmail.com>
2024-05-23 01:28:13 +0900

    Feat: "Spring Security 인증/인가"
    로그인 시도 시 인증 처리 관련 추가 구현
    - Authentication Manager에서 인증 로직 수행을 위한 UserDetailsService 및 UserDetails 구현
    - 인증 토큰 생성 시 사용된 employeeCode(username)로 DB에서 직원 조회 후 userDetails 객체 반환

--- src/main/java/org/iot/hotelitybackend/login/service/LoginServiceImpl.java
+++ src/main/java/org/iot/hotelitybackend/login/service/LoginServiceImpl.java
@@ -1,4 +1,27 @@
 package org.iot.hotelitybackend.login.service;
 
-public class LoginServiceImpl {
+import org.iot.hotelitybackend.employee.aggregate.EmployeeEntity;
+import org.iot.hotelitybackend.employee.repository.EmployeeRepository;
+import org.iot.hotelitybackend.login.vo.LoginUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public LoginServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        EmployeeEntity employee = employeeRepository.findById(Integer.valueOf(username)).orElseThrow();
+
+        return new LoginUserDetails(employee);
+    }
 }
---
+++ src/main/java/org/iot/hotelitybackend/login/vo/LoginUserDetails.java
@@ -0,0 +1,55 @@
+package org.iot.hotelitybackend.login.vo;
+
+import org.iot.hotelitybackend.employee.aggregate.EmployeeEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class LoginUserDetails implements UserDetails {
+
+    private final EmployeeEntity employeeEntity;
+
+    public LoginUserDetails(EmployeeEntity employeeEntity) {
+        this.employeeEntity = employeeEntity;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add((GrantedAuthority) employeeEntity::getPermissionName);
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return employeeEntity.getEmployeeSystemPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return String.valueOf(employeeEntity.getEmployeeCodePk());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
