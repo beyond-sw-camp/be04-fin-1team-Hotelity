694a9627636176232e779365f8f01ec3633f2923
jhlee <jun73045928@gmail.com>
2024-05-23 00:01:06 +0900

    Feat: "Spring Security 인증/인가"
    Security Config 추가

--- src/main/java/org/iot/hotelitybackend/login/security/WebSecurityConfig.java
+++ src/main/java/org/iot/hotelitybackend/login/security/WebSecurityConfig.java
@@ -1,4 +1,55 @@
 package org.iot.hotelitybackend.login.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity(debug = true)
 public class WebSecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Autowired
+    public WebSecurityConfig(
+            AuthenticationConfiguration authenticationConfiguration
+    ) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain configureSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        // csrf disable
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // form login disable
+        http.formLogin(AbstractHttpConfigurer::disable);
+
+        // http basic disable
+        http.httpBasic(AbstractHttpConfigurer::disable);
+
+        // 인가(Authorization)
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        // 세션 설정
+        http.sessionManagement(httpSecuritySessionManagementConfigurer ->
+                httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 }
