38da415d84c169986529f26c6744aa09ef67a041
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-05-23 15:35:47 +0900

    Refactor: "쿠폰 종류 전체 조회"
    쿠폰 종류 전체 조회 기능 수정
    
    Issues: #159

--- src/main/java/org/iot/hotelitybackend/sales/service/CouponService.java
+++ src/main/java/org/iot/hotelitybackend/sales/service/CouponService.java
@@ -1,18 +1,17 @@
 package org.iot.hotelitybackend.sales.service;
 
-import org.iot.hotelitybackend.sales.dto.CouponDTO;
 import org.iot.hotelitybackend.sales.vo.RequestCoupon;
 
+import java.util.Date;
 import java.util.Map;
 
 public interface CouponService {
-    Map<String, Object> selectAllCouponsType(int pageNum);
-
-    CouponDTO selectCouponByCouponCodePk(int couponCodePk);
+    Map<String, Object> selectAllCouponsType(Integer pageNum, Integer couponCodePk, String couponName, String couponType,
+		Double couponDiscountRate, Date couponLaunchingDate, String couponInfo, Integer membershipLevelCodeFk,
+        String orderBy, Integer sortBy);
 
     Map<String, Object> registCoupon(RequestCoupon requestCoupon);
 
     Map<String, Object> modifyCoupon(RequestCoupon requestCoupon, int couponCodePk);
 
-    Map<String, Object> selectSearchedCouponsList(int pageNum, Float couponDiscountRate, String couponType);
 }
