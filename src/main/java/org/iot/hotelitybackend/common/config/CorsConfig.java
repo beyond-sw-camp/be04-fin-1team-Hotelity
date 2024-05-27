c1195daef9bed91395cc3f480e5574b53d92b848
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-05-27 14:42:16 +0900

    Feat: "국가 정보 가져오기"
    국가 정보 리스트 가져오기 구현 완료.

--- src/main/java/org/iot/hotelitybackend/common/config/CorsConfig.java
+++ src/main/java/org/iot/hotelitybackend/common/config/CorsConfig.java
@@ -1,27 +1,24 @@
 package org.iot.hotelitybackend.common.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
+
 	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration config = new CorsConfiguration();
-
-		config.setAllowCredentials(true);
-		config.setAllowedOrigins(List.of("*"));
-		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-		config.setAllowedHeaders(List.of("*"));
-		config.setExposedHeaders(List.of("*"));
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config);
-		return source;
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins("http://localhost:5173") // Allowing the specific origin
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+					.allowedHeaders("*")
+					.allowCredentials(true);
+			}
+		};
 	}
 }
---
+++ src/main/java/org/iot/hotelitybackend/customer/dto/NationDTO.java
@@ -0,0 +1,9 @@
+package org.iot.hotelitybackend.customer.dto;
+
+import lombok.Data;
+
+@Data
+public class NationDTO {
+	public Integer nationCodePk;
+	public String nationName;
+}
