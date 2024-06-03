5d7d50a560f2807879110dceb6fdb01e37db35c7
universe <hello.its.universe@gmail.com>
2024-06-03 11:52:43 +0900

    Refactor: "투숙 일자별 리스트 조회 기능"
    투숙 체크인 시간을 기준으로 조회하도록 리팩토링
    
    Refactor: #205

--- src/main/java/org/iot/hotelitybackend/hotelservice/repository/StayRepository.java
+++ src/main/java/org/iot/hotelitybackend/hotelservice/repository/StayRepository.java
@@ -1,5 +1,7 @@
 package org.iot.hotelitybackend.hotelservice.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.iot.hotelitybackend.hotelservice.aggregate.StayEntity;
@@ -9,4 +11,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 public interface StayRepository extends JpaRepository<StayEntity, Integer>, JpaSpecificationExecutor<StayEntity> {
 	Optional<StayEntity> findByReservationCodeFk(int reservationCodePk);
 
+	List<StayEntity> findAllByStayCheckinTimeBetween(LocalDateTime start, LocalDateTime end);
 }
