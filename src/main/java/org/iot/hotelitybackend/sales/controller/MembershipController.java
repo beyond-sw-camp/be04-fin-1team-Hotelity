40346876e815275a34e665277bd21ea30bfcfbe4
universe <hello.its.universe@gmail.com>
2024-06-06 14:24:19 +0900

    Feat: "멤버십 전체 조회 시 페이징 추가"
    멤버십 전체 조회 시 페이징 추가

--- src/main/java/org/iot/hotelitybackend/sales/controller/MembershipController.java
+++ src/main/java/org/iot/hotelitybackend/sales/controller/MembershipController.java
@@ -1,17 +1,17 @@
 package org.iot.hotelitybackend.sales.controller;
 
+import org.iot.hotelitybackend.common.vo.ResponseVO;
 import org.iot.hotelitybackend.customer.dto.CustomerDTO;
 import org.iot.hotelitybackend.sales.dto.MembershipDTO;
 import org.iot.hotelitybackend.sales.repository.MembershipRepository;
 import org.iot.hotelitybackend.sales.service.MembershipService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/sales")
@@ -25,7 +25,14 @@ public class MembershipController {
     }
 
     @GetMapping("/membership")
-    public List<MembershipDTO> selectAllMembership() {
-        return membershipService.selectAllMembership();
+    public ResponseEntity<ResponseVO> selectAllMembership(@RequestParam(required = false) Integer pageNum) {
+        Map<String, Object> membershipPageInfo = membershipService.selectAllMembership(pageNum);
+
+        ResponseVO response = ResponseVO.builder()
+                .data(membershipPageInfo)
+                .resultCode(HttpStatus.OK.value())
+                .build();
+
+        return ResponseEntity.status(response.getResultCode()).body(response);
     }
 }
--- src/main/java/org/iot/hotelitybackend/sales/service/MembershipService.java
+++ src/main/java/org/iot/hotelitybackend/sales/service/MembershipService.java
@@ -4,8 +4,9 @@ package org.iot.hotelitybackend.sales.service;
 import org.iot.hotelitybackend.sales.dto.MembershipDTO;
 
 import java.util.List;
+import java.util.Map;
 
 public interface MembershipService {
 
-    List<MembershipDTO> selectAllMembership();
+    Map<String, Object> selectAllMembership(Integer pageNum);
 }
